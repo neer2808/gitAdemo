9a16f054c02fea23eaef33e2e99f69cc6bdb9dd5
neer2808 <uic.neerajkhanna@gmail.com>
2021-09-14 15:40:38 +0530

    add various files

---
+++ src/Demo78.java
@@ -0,0 +1,19 @@
+public class Demo78 {
+  private int num1;
+  private int num2;
+
+  public Demo78(int num1, int num2) {
+    this.num1 = num1;
+    this.num2 = num2;
+  }
+  public void show()
+  {
+    System.out.println(num1);
+    System.out.println(num2);
+  }
+
+  public static void main(String[] args) {
+    Demo78 obj = new Demo78(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+   obj.show();
+  }
+}
