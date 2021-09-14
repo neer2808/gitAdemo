9619c5f2072005b604d6fcfe4414cade0c4e4b5d
neer2808 <uic.neerajkhanna@gmail.com>
2021-09-14 14:30:35 +0530

    added one non parameterised constructor

--- src/Demmo345.java
+++ src/Demmo345.java
@@ -2,4 +2,8 @@ public class Demmo345 {
   public static void main(String[] args) {
     System.out.println("welcome to java ");
   }
+  Demmo345()
+  {
+    System.out.println("inside constructor ");
+  }
 }
